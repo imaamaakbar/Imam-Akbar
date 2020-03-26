2fd4f19aa8cc3f9c6ce7e35f4446b0979565fb6d
imaamaakbar <62373372+imaamaakbar@users.noreply.github.com>
2020-03-26 15:22:08 +0700

    Update Operasi Sederhana Statistika.cpp

--- Operasi Sederhana Statistika.cpp
+++ Operasi Sederhana Statistika.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
+#include<conio.h>
 using namespace std;
 //fungsi mean
 void mean(int *pA, int n ){
-	
 	float sumxbar=0;
 	//mencari jumlah total
  for (int i=0;i<n;i++){
- 	sumxbar=*(pA+i)+sumxbar;
- 		
+ 	sumxbar=*(pA+i)+sumxbar;		
  }
  // mencari rata rata
  float xbar=sumxbar/n;
@@ -30,13 +29,10 @@ void median (int n, int *pA){
             }
         }
     }
-	
 	 cout<<" jika diurutkan maka : ";
 	 for(int i=0;i<n;i++){
-	 	cout<<*(pA+i)<<" ";
-	 	
+	 	cout<<*(pA+i)<<" ";	
 	 }
-	 
 	 cout<<endl;
 	 //mencari median
 	if(n%2==0){
@@ -47,6 +43,25 @@ void median (int n, int *pA){
 	else{
 	    med=*(pA+(n/2));
 	}
-	cout<<"Median = "<<med<<endl;
-	      
+	cout<<"Median = "<<med<<endl;	      
 	}
+void modus(int *pA, int n){
+int mod[100];
+int total[100];
+int x,k=1;
+x=0;
+//mengurutkan
+	int asc;
+	for(int c=1;c<n;c++)
+    {
+        for(int i=0;i<n-c;i++)
+        {
+            if(*(pA+i) > *(pA+(i+1)))
+            {
+                asc=*(pA+i);
+                *(pA+i)=*(pA+(i+1));
+                *(pA+(i+1))=asc;
+            }
+        }
+    }
+	
