c78ad55612fb332cbf727a5099054a9bdd3d1839
imaamaakbar <62373372+imaamaakbar@users.noreply.github.com>
2020-03-26 15:16:16 +0700

    Update Operasi Sederhana Statistika.cpp

--- Operasi Sederhana Statistika.cpp
+++ Operasi Sederhana Statistika.cpp
@@ -1,2 +1,52 @@
 #include <iostream>
 using namespace std;
+//fungsi mean
+void mean(int *pA, int n ){
+	
+	float sumxbar=0;
+	//mencari jumlah total
+ for (int i=0;i<n;i++){
+ 	sumxbar=*(pA+i)+sumxbar;
+ 		
+ }
+ // mencari rata rata
+ float xbar=sumxbar/n;
+ cout<<"Rata-rata/Xbar = "<<xbar;
+}
+//fungsi median
+void median (int n, int *pA){
+	float med=0.0;
+	//mengurutkan
+	int asc;
+	for(int c=1;c<n;c++)
+    {
+        for(int i=0;i<n-c;i++)
+        {
+            if(*(pA+i) > *(pA+(i+1)))
+            {
+                asc=*(pA+i);
+                *(pA+i)=*(pA+(i+1));
+                *(pA+(i+1))=asc;
+            }
+        }
+    }
+	
+	 cout<<" jika diurutkan maka : ";
+	 for(int i=0;i<n;i++){
+	 	cout<<*(pA+i)<<" ";
+	 	
+	 }
+	 
+	 cout<<endl;
+	 //mencari median
+	if(n%2==0){
+		float sum;
+		sum=(*(pA+(n/2))+*(pA+((n/2)-1)));
+    med=sum/2;
+	}
+	else{
+	    med=*(pA+(n/2));
+	}
+	cout<<"Median = "<<med<<endl;
+	      
+	}
